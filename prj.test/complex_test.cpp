6d3e54ce6e9736dc06b8e27e30484c32c1056760
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2024-11-06 00:40:50 +0300

    Update complex_test.cpp

--- prj.test/complex_test.cpp
+++ prj.test/complex_test.cpp
@@ -1,1 +1,35 @@
+#include <complex/complex.hpp>
 
+
+int main() {
+	
+	setlocale(LC_ALL, "Russian");
+
+
+
+	Complex a(0, 0), b(0, 0);
+	std::cin >> a >> b;
+
+
+	bool sz = a == b;
+	std::cout << a << " = " << b << ": " << sz << std::endl;
+
+	Complex sm = a + b;
+	std::cout << sm << std::endl;
+
+
+	Complex rz = a - b;
+	std::cout << rz << std::endl;
+
+	Complex ymn = a * b;
+	std::cout << ymn << " - Ymnoshenie" << std::endl;
+
+	Complex delen = a / b;
+	std::cout << delen << std::endl;
+
+	std::cout << a.info() << std::endl;
+
+	return 0;
+
+
+}
