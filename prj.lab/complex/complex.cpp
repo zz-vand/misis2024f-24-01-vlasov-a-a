424dbcbd33044fa0327d0f77b00f271c63a345bc
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2024-11-06 00:36:46 +0300

    Update complex.cpp

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -26,6 +26,7 @@ Complex& Complex::operator-=(const double rhs) noexcept {
 	re -= rhs;
 	return *this; }
 
+//Формула: (a+bi)(c+di) = (ac-bd) + (ad+bc)i
 Complex& Complex::operator*=(const Complex& rhs) noexcept {  
 	re = re * rhs.re - im * rhs.im;
 	im = re * rhs.im + im * rhs.re;
@@ -46,10 +47,41 @@ Complex& Complex::operator/=(const double rhs) {
 	im /= rhs;
 	return *this; }
 
-std::ostream& Complex::WriteTo(std::ostream& ostrm) const noexcept { 
-	return ostrm; }
-std::istream& Complex::ReadFrom(std::istream& istrm) noexcept { 
-	return istrm; }
+std::ostream & Complex::writeTo(std::ostream & ostrm) const
+ {
+	 ostrm << lb << re << sep << im << rb;
+	 return ostrm;
+	}
+
+std::istream& Complex::readFrom(std::istream& istrm)
+{
+	char leftBrace(0);
+	double real(0.0);
+	char comma(0);
+	double imaganary(0.0);
+	char rightBrace(0);
+	istrm >> leftBrace >> real >> comma >> imaganary >> rightBrace;
+	if (istrm.good()) {
+		if ((Complex::lb == leftBrace) && (Complex::sep == comma)
+			&& (Complex::rb == rightBrace)) {
+			re = real;
+			im = imaganary;
+
+		}
+		else {
+			istrm.setstate(std::ios_base::failbit);
+
+		}
+
+	}
+	return istrm;
+}
+
+
+
+
+
+
 
 
 Complex operator+(const Complex& lhs, const Complex& rhs) noexcept { 
