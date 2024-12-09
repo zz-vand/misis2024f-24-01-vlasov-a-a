db0537bf132e553fd5c853c65859397eae27f81e
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2024-12-09 09:51:32 +0300

    Create array_test.cpp

---
+++ prj.test/array_test.cpp
@@ -0,0 +1,22 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <arrayd/arrayd.hpp>
+
+TEST_CASE("[arrayd] - ArrayD ctor") {
+    ArrayD arr1(1);
+    CHECK(arr1.ssize() == 1);
+    CHECK(arr1.capacity() == 1);
+
+    ArrayD arr2(arr1);
+    CHECK(arr2[0] == 0.0);
+
+    ArrayD arr(0);
+    CHECK(arr.ssize() == 0);
+}
+
+TEST_CASE("[arrayd] - ArrayD insert") {
+    ArrayD arr(1);
+    arr.insert(1, 2.5);
+    CHECK(arr[1] == 2.5);
+}
