1a19b5ead7f87191818b23291eceff69be9f09c2
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2025-01-08 02:00:31 +0300

    Update arrayt_doctest.cpp

--- prj.test/arrayt_doctest.cpp
+++ prj.test/arrayt_doctest.cpp
@@ -5,11 +5,82 @@
 
 #include <string>
 
-TEST_CASE("[arrayt] - ArrayT ctor") {
-    ArrayT<int> arr(1);
+TEST_CASE("") {
+    ArrayT<char> arr(1);
     CHECK(arr[0] == 0);
 
-    ArrayT<std::string> arr1(1);
-    arr1.resize(4);
-    CHECK(arr1[3] == "");
+    arr.resize(4);
+    CHECK(arr.ssize() == 4);
+
+    arr.insert(1, 'a');
+    CHECK(arr[1] == 'a');
+
+    arr.remove(1);
+    CHECK(arr.ssize() == 4);
+    CHECK(arr[1] != 'a');
+
+    arr.insert(3, 9);
+    arr.remove(2);
+    CHECK(arr[2] == 9);
+
+}
+
+TEST_CASE("") {
+    ArrayT<float> arr(1);
+    CHECK(arr[0] == 0);
+
+    arr.resize(4);
+    CHECK(arr.ssize() == 4);
+
+    arr.insert(1, 1.8);
+    CHECK(arr[1] == 1.8f);
+
+    arr.remove(1);
+    CHECK(arr.ssize() == 4);
+    CHECK(arr[1] == 0);
+
+    arr.insert(3, 9.72);
+    arr.remove(2);
+    CHECK(arr[2] == 9.72f);
+
+}
+
+TEST_CASE("") {
+    ArrayT<std::string> arr(1);
+    CHECK(arr[0] == "");
+
+    arr.resize(4);
+    CHECK(arr.ssize() == 4);
+
+    arr.insert(1, "cool");
+    CHECK(arr[1] == "cool");
+
+    arr.remove(1);
+    CHECK(arr.ssize() == 4);
+    CHECK(arr[1] == "");
+
+    arr.insert(3, "snow");
+    arr.remove(2);
+    CHECK(arr[2] == "snow");
+
+}
+
+TEST_CASE("") {
+    ArrayT<double> arr(1);
+    CHECK(arr[0] == 0);
+
+    arr.resize(4);
+    CHECK(arr.ssize() == 4);
+
+    arr.insert(1, 1.8);
+    CHECK(arr[1] == 1.8);
+
+    arr.remove(1);
+    CHECK(arr.ssize() == 4);
+    CHECK(arr[1] == 0);
+
+    arr.insert(3, 9.72);
+    arr.remove(2);
+    CHECK(arr[2] == 9.72);
+
 }
