90b3423ace9981f36b6359a8cc5f0fce1dd36a77
AndrewVlasov-st <130500927+zz-vand@users.noreply.github.com>
2025-01-08 02:02:06 +0300

    Update rational_doctest.cpp

--- prj.test/rational_doctest.cpp
+++ prj.test/rational_doctest.cpp
@@ -6,14 +6,14 @@
 #include <sstream>
 
 TEST_CASE("[rational] - Rational ctor") {
-  CHECK(Rational() == Rational(0, 1));
-  CHECK(Rational(3) == Rational(3, 1));
-  CHECK(Rational(-3) == Rational(-3, 1));
-  CHECK(Rational(10, 6) == Rational(5, 3));
-  CHECK(Rational(-10, 6) == Rational(-5, 3));
-  CHECK(Rational(10, -6) == Rational(-5, 3));
-  CHECK(Rational(-10, -6) == Rational(5, 3));
-  CHECK_THROWS(Rational(1, 0));
+    CHECK(Rational() == Rational(0, 1));
+    CHECK(Rational(3) == Rational(3, 1));
+    CHECK(Rational(-3) == Rational(-3, 1));
+    CHECK(Rational(10, 6) == Rational(5, 3));
+    CHECK(Rational(-10, 6) == Rational(-5, 3));
+    CHECK(Rational(10, -6) == Rational(-5, 3));
+    CHECK(Rational(-10, -6) == Rational(5, 3));
+    CHECK_THROWS(Rational(1, 0));
 }
 
 TEST_CASE("[rational] - Rational add") {
